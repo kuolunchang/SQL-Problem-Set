f41e1489e0fb9d0129eebb3ddf9ec2a5e1aeff69
Kuolun Chang <70204598+kuolunchang@users.noreply.github.com>
2021-11-23 17:03:24 -0500

    Update find_updated_records.sql

--- Easy/find_updated_records.sql
+++ Easy/find_updated_records.sql
@@ -1,5 +1,6 @@
 -- Finding Updated Records
--- We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+-- We have a table with employees and their salaries.
+-- However, some of the records are old and contain outdated salary information. 
 -- Find the current salary of each employee assuming that salaries increase each year. 
 -- Output their id, first name, last name, department ID, and current salary. 
 -- Order your list by employee ID in ascending order.
