2dfe54f6ece2a19073bc4ab59ceb92c6f30f10b7
Kuolun Chang <70204598+kuolunchang@users.noreply.github.com>
2021-11-23 17:02:21 -0500

    Add files via upload

---
+++ Easy/find_updated_records.sql
@@ -0,0 +1,25 @@
+-- Finding Updated Records
+-- We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+-- Find the current salary of each employee assuming that salaries increase each year. 
+-- Output their id, first name, last name, department ID, and current salary. 
+-- Order your list by employee ID in ascending order.
+
+-- Table: ms_employee_salary
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | first_name    | varchar |
+-- | last_name     | varchar |
+-- | salary        | int     |
+-- | department_id | int     |
+-- +---------------+---------+
+
+
+--------------------------------------------------------------------
+
+SELECT id, first_name, last_name,max(salary), department_id 
+FROM ms_employee_salary 
+GROUP BY id, first_name, last_name, department_id
+ORDER BY id
